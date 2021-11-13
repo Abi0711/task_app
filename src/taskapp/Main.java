20175786d822e0ad0377d7bc6d8fce40a50eaf3b
Abi0711 <a.f.tian0711@gmail.com>
2021-11-13 22:10:42 +1100

    Refactored file names

--- src/taskapp/Main.java
+++ src/taskapp/Main.java
@@ -1,25 +1,15 @@
-package sample;
+package taskapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
 
-import javax.swing.text.html.ImageView;
 import java.util.ArrayList;
 
 public class Main extends Application {
@@ -27,26 +17,7 @@ public class Main extends Application {
     ArrayList<Label> allTasks = new ArrayList<>();
     @Override
     public void start(Stage stage) throws Exception{
-//        stage.setTitle("Task App");
-////        Group root = new Group();
-////        Scene scene = new Scene(root, 300,300);
-////        stage.setScene(scene);
-////        Rectangle r = new Rectangle(100,100,100,100);
-////        r.setFill(Color.RED);
-////        root.getChildren().add(r);
-//        StackPane rootText = new StackPane();
-//        Scene sceneText = new Scene(rootText, 600,700);
-//        stage.setScene(sceneText);
-//        Text hi = new Text("Hello World");
-//        hi.setFont(Font.font("Tahoma", FontWeight.BLACK, 40));
-//        hi.setFill(Color.ROYALBLUE);
-//        //stage.setOpacity(0.5);
-//        rootText.getChildren().add(hi);
-//        stage.show();
-//        TextArea textarea = new TextArea();
-//        Group group = new Group();
-//        group.getChildren().addAll(textarea);
-
+        stage.setTitle("Task App");
 
         TextArea textarea = new TextArea();
         Group group = new Group();
