d17d442704bf399d36c9a9e8570b504b527841ce
Abi0711 <a.f.tian0711@gmail.com>
2022-01-08 22:14:47 +1100

    Cleaned up code and changed Labels to Tasks

--- src/taskapp/Controller.java
+++ src/taskapp/Controller.java
@@ -1,39 +1,21 @@
 package taskapp;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 
 public class Controller {
+
     public static VBox createTopic() {
-        VBox hbox = new VBox();
+        VBox topic = new VBox();
 
-        Label l1 = new Label("Label 1");
-        l1.setAlignment(Pos.CENTER);
-        l1.setBackground(new Background(new BackgroundFill(Color.ROYALBLUE, new CornerRadii(5.0), new Insets(5, 5,5,5))));
-        l1.setMaxWidth(Double.MAX_VALUE);
-        //l1.setMaxHeight(Double.MAX_VALUE);
+        Task l1 = new Task("Task 1");
         VBox.setVgrow(l1, Priority.ALWAYS);
+        
+        Task l2 = new Task("Task 2");
+        Task l3 = new Task("Task 3");
+        Task l4 = new Task("Task 4");
+        Task l5 = new Task("Task 5");
 
-        hbox.setFillWidth(true);
-
-
-        Label l2 = new Label("Label 2");
-        l2.setMaxWidth(200);
-        //l2.setMaxHeight(200);
-        l2.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY, new CornerRadii(5.0), new Insets(5, 5,5,5))));
-
-        Label l3 = new Label("Label 3");
-        l3.setMaxWidth(200);
-
-        Label l4 = new Label("Label 4");
-
-
-        Label l5 = new Label("Label 5");
-
-        hbox.getChildren().addAll(l1, l2, l3, l4, l5);
-        return hbox;
+        topic.getChildren().addAll(l1, l2, l3, l4, l5);
+        return topic;
     }
 }
--- src/taskapp/Main.java
+++ src/taskapp/Main.java
@@ -2,7 +2,9 @@ package taskapp;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
@@ -20,19 +22,32 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         stage.setTitle("Task App");
+        HBox topicContainer = new HBox();
+        Button addTopic = new Button("+ New Topic");
+        topicContainer.getChildren().add(addTopic);
+
+        addTopic.setOnAction(actionEvent ->  {
+            VBox t1 = Controller.createTopic();
+            HBox.setHgrow(t1, Priority.ALWAYS);
+            topicContainer.getChildren().add(0,t1);
+        });
 
         TextArea textarea = new TextArea();
         Group group = new Group();
         group.getChildren().addAll(textarea);
 
-        Scene scene = new Scene(group, 640, 480);
+
+
+        Scene scene = new Scene(topicContainer, 640, 480);
         stage.setScene(scene);
         stage.show();
         textarea.setOnKeyPressed( event -> {
             if( event.getCode() == KeyCode.ENTER ) {
+
                 Label txtField = new Label();
                 txtField.setText(textarea.getText());
-                Task t = new Task(textarea.getText(), txtField);
+                Task t = new Task(textarea.getText());
+
                 group.getChildren().add(t);
                 allTasks.add(t);
             }
@@ -47,28 +62,20 @@ public class Main extends Application {
 }
 
 class Task extends Label {
-    String taskName;
     private double mousex;
     private double mousey;
     //String description;
     boolean complete;
-    private Label text;
     //int deadline or dateDue
 
-    Task(String taskName, Label t) {
-        this.setLayoutX(300);
-        this.setLayoutY(300);
-        Insets o = new Insets(7);
-        CornerRadii c = new CornerRadii(70);
-        Border b = new Border((new BorderStroke(Color.BLACK,
-                BorderStrokeStyle.SOLID, c, new BorderWidths(2), o)));
-
-        this.setBorder(b);
+    Task(String taskName) {
+        this.setAlignment(Pos.CENTER);
+        this.setBackground(new Background(new BackgroundFill(Color.ROYALBLUE, new CornerRadii(5.0), new Insets(5, 5,5,5))));
+        this.setMaxWidth(Double.MAX_VALUE);
         this.setText(taskName);
 
-        this.taskName = taskName;
         this.complete = false;
-        this.text = t;
+        this.setText(taskName);
 
         this.setOnMousePressed(event -> {
             mousex = this.getLayoutX() - event.getSceneX();
@@ -84,31 +91,5 @@ class Task extends Label {
             mousey = event.getSceneY();
         });
         this.toFront();
-
-//        Node textAreaContent = text.lookup(".content");
-//        textAreaContent.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
-//
-//            System.out.println("is clicked");
-//
-//            orgSceneX = e.getSceneX();
-//            orgSceneY = e.getSceneY();
-//            orgTranslateX = text.getTranslateX();
-//            orgTranslateY = text.getTranslateY();
-//
-//            text.toFront();
-//        });
-//
-//        textAreaContent.addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> {
-//
-//            System.out.println("is dragged");
-//
-//            double offsetX = e.getSceneX() - orgSceneX;
-//            double offsetY = e.getSceneY() - orgSceneY;
-//            double newTranslateX = orgTranslateX + offsetX;
-//            double newTranslateY = orgTranslateY + offsetY;
-//
-//            text.setTranslateX(newTranslateX);
-//            text.setTranslateY(newTranslateY);
-//        });
     }
 }
