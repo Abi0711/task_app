57fc9cd5ab87fb32e329a22eb1c2a1558860287b
Abi0711 <a.f.tian0711@gmail.com>
2022-01-08 23:04:27 +1100

    Can add tasks to topics and add topics

--- src/taskapp/Controller.java
+++ src/taskapp/Controller.java
@@ -1,21 +1,39 @@
 package taskapp;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class Controller {
 
     public static VBox createTopic() {
         VBox topic = new VBox();
-
+        topic.setBackground(new Background(new BackgroundFill(Color.BLACK,  new CornerRadii(10.0), new Insets(3))));
         Task l1 = new Task("Task 1");
-        VBox.setVgrow(l1, Priority.ALWAYS);
+        //VBox.setVgrow(l1, Priority.ALWAYS);
         
         Task l2 = new Task("Task 2");
         Task l3 = new Task("Task 3");
         Task l4 = new Task("Task 4");
         Task l5 = new Task("Task 5");
 
-        topic.getChildren().addAll(l1, l2, l3, l4, l5);
+        Button addTask = new Button("+");
+
+        addTask.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY,  new CornerRadii(10.0), new Insets(2))));
+        addTask.setAlignment(Pos.CENTER);
+        addTask.setMaxWidth(Double.MAX_VALUE);
+        addTask.setFont(Font.font("Arial", 20));
+
+        topic.getChildren().addAll(l1, l2, l3, l4, l5, addTask);
+
+
+        addTask.setOnAction(actionEvent ->  {
+
+        });
+
         return topic;
     }
 }
--- src/taskapp/Main.java
+++ src/taskapp/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.control.TextArea;
@@ -24,10 +25,12 @@ public class Main extends Application {
         stage.setTitle("Task App");
         HBox topicContainer = new HBox();
         Button addTopic = new Button("+ New Topic");
+        topicContainer.setAlignment(Pos.CENTER);
         topicContainer.getChildren().add(addTopic);
 
         addTopic.setOnAction(actionEvent ->  {
-            VBox t1 = Controller.createTopic();
+            //VBox t1 = Controller.createTopic();
+            Topic t1 = new Topic();
             HBox.setHgrow(t1, Priority.ALWAYS);
             topicContainer.getChildren().add(0,t1);
         });
@@ -61,6 +64,56 @@ public class Main extends Application {
     }
 }
 
+class Topic extends VBox{
+    //whether the user is editing a textbox currently
+    boolean editing;
+    TextArea newTask;
+
+    Topic(){
+        this.setBackground(new Background(new BackgroundFill(Color.BLACK,  new CornerRadii(10.0), new Insets(3))));
+        newTask = new TextArea();
+        //TODO set text box font and styling
+
+        Task l1 = new Task("Task 1");
+        //VBox.setVgrow(l1, Priority.ALWAYS);
+
+        Task l2 = new Task("Task 2");
+        Task l3 = new Task("Task 3");
+        Task l4 = new Task("Task 4");
+        Task l5 = new Task("Task 5");
+
+        Button addTask = new Button("+");
+
+        addTask.setBackground(new Background(new BackgroundFill(Color.LIGHTGREY,  new CornerRadii(10.0), new Insets(2))));
+        addTask.setAlignment(Pos.CENTER);
+        addTask.setMaxWidth(Double.MAX_VALUE);
+        addTask.setFont(Font.font("Arial", 20));
+
+        this.getChildren().addAll(l1, l2, l3, l4, l5, addTask);
+
+
+        addTask.setOnAction(actionEvent ->  {
+            if(!editing){
+                this.getChildren().add(this.getChildren().size()-1,newTask);
+                System.out.println("Can now edit");
+                //TODO set focus to textarea
+            }
+            else{
+                System.out.println("Already adding something");
+            }
+        });
+        newTask.setOnKeyPressed( event -> {
+            if( event.getCode() == KeyCode.ENTER ) {
+                Task temp = new Task(newTask.getText());
+                this.getChildren().remove(newTask);
+                this.getChildren().add(this.getChildren().size()-1,temp);
+                newTask.setText("");
+                editing = false;
+            }
+        } );
+    }
+}
+
 class Task extends Label {
     private double mousex;
     private double mousey;
@@ -70,13 +123,15 @@ class Task extends Label {
 
     Task(String taskName) {
         this.setAlignment(Pos.CENTER);
-        this.setBackground(new Background(new BackgroundFill(Color.ROYALBLUE, new CornerRadii(5.0), new Insets(5, 5,5,5))));
+        this.setBackground(new Background(new BackgroundFill(Color.ROYALBLUE, new CornerRadii(5.0), new Insets(5))));
         this.setMaxWidth(Double.MAX_VALUE);
         this.setText(taskName);
-
+        this.setFont(Font.font("Arial", 30));
+        this.setTextFill(Color.color(1,1,1));
         this.complete = false;
-        this.setText(taskName);
 
+
+        //TODO drag tasks around
         this.setOnMousePressed(event -> {
             mousex = this.getLayoutX() - event.getSceneX();
             mousey = this.getLayoutY() - event.getSceneY();
